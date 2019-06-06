bff4432d9a278ad0d34e9bf9454f02f7b223da0c
NickBarros10 <1johndoegithub@gmail.com>
2019-06-06 12:52:52 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,106 @@
+<template>
+  <div id="app">
+    <!-- nav bar -->
+    <template>
+  <!--Navbar-->
+  <mdb-navbar color="blue" dark>
+    <!-- Navbar brand -->
+    <mdb-navbar-brand href="https://mdbootstrap.com/">
+      Navbar
+    </mdb-navbar-brand>
+    <mdb-navbar-toggler>
+      <mdb-navbar-nav>
+        <mdb-nav-item href="#" active>Home</mdb-nav-item>
+        <mdb-nav-item href="#">Features</mdb-nav-item>
+        <mdb-nav-item href="#">Pricing</mdb-nav-item>
+        <!-- Dropdown -->
+        <mdb-dropdown tag="li" class="nav-item">
+          <mdb-dropdown-toggle tag="a" navLink color="blue" slot="toggle" waves-fixed>Dropdown</mdb-dropdown-toggle>
+          <mdb-dropdown-menu>
+            <mdb-dropdown-item>Action</mdb-dropdown-item>
+            <mdb-dropdown-item>Another action</mdb-dropdown-item>
+            <mdb-dropdown-item>Something else here</mdb-dropdown-item>
+          </mdb-dropdown-menu>
+        </mdb-dropdown>
+      </mdb-navbar-nav>
+      <!-- Search form -->
+      <mdb-form-inline>
+        <mdb-input type="text" placeholder="Search" aria-label="Search"/>
+      </mdb-form-inline>
+    </mdb-navbar-toggler>
+  </mdb-navbar>
+</template>
+    <router-view>
+    </router-view>
+      
+  
+<template>
+  <!-- Footer -->
+  <mdb-footer color="blue" class="font-small pt-0 foot">
+    <mdb-container>
+      <mdb-row>
+        <mdb-col md="12">
+          <div class="mb-5 flex-center">
+            <a class="fb-ic"><i class="fab fa-facebook fa-lg white-text mr-md-5 mr-3 fa-2x"> </i></a>
+            <a class="tw-ic"><i class="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i></a>
+            <a class="gplus-ic"><i class="fab fa-google-plus fa-lg white-text mr-md-5 mr-3 fa-2x"> </i></a>
+            <a class="li-ic"><i class="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x"> </i></a>
+            <a class="ins-ic"><i class="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i></a>
+            <a class="pin-ic"><i class="fab fa-pinterest fa-lg white-text fa-2x"> </i></a>
+          </div>
+        </mdb-col>
+      </mdb-row>
+    </mdb-container>
+    <div class="footer-copyright text-center py-3">
+      <mdb-container fluid>
+        &copy; 2018 Copyright: <a href="https://www.MDBootstrap.com"> MDBootstrap.com </a>
+      </mdb-container>
+    </div>
+  </mdb-footer>
+  <!-- Footer -->
+</template>
+    
+  </div>
+</template>
+
+<script>
+import { mdbFooter, mdbContainer, mdbRow, mdbCol, mdbNavbar, mdbNavbarBrand, mdbNavbarToggler, mdbNavbarNav, mdbNavItem, mdbDropdown, mdbDropdownMenu, mdbDropdownToggle, mdbInput, mdbDropdownItem, mdbFormInline } from 'mdbvue';
+
+export default {
+  components: {
+      mdbFooter,
+      mdbContainer,
+      mdbRow,
+      mdbCol,
+      mdbNavbar,
+      mdbNavbarBrand,
+      mdbNavbarToggler,
+      mdbNavbarNav,
+      mdbNavItem,
+      mdbDropdown,
+      mdbDropdownMenu,
+      mdbDropdownToggle,
+      mdbDropdownItem,
+      mdbFormInline,
+      mdbInput
+    },
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  overflow-x: hidden;
+}
+
+.foot{
+  margin-top: 300px !important;
+  text-align: center !important;
+}
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="hello">
+    <img class="logo" src="../assets/logo.png">
+    <h3>{{ msg }}</h3>
+
+    <template>
+  <mdb-container>
+    <mdb-row>
+      <mdb-col col="4">col="4"</mdb-col>
+      <mdb-col col="4">col="4"</mdb-col>
+      <mdb-col col="4">col="4"</mdb-col>
+    </mdb-row>
+  </mdb-container>
+</template>
+
+<mdb-container>
+<img src="https://mdbootstrap.com/img/Photos/Slides/img%20(54).jpg" class="img-fluid z-depth-1 picture-width" alt="1">
+<img src="https://mdbootstrap.com/img/Photos/Slides/img%20(54).jpg" class="img-fluid z-depth-1 picture-width" alt="1">
+</mdb-container>
+
+
+
+
+  </div>
+</template>
+
+<script>
+import { mdbFooter, mdbContainer, mdbRow, mdbCol} from 'mdbvue';
+  
+export default {  
+  components: {
+      mdbContainer,
+      mdbRow,
+      mdbCol,
+      mdbFooter
+    },
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Our Website'
+    };
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  font-weight: normal;
+  padding-top: 20px;
+  padding-bottom: 30px;
+}
+p {
+  color: #969696;
+  margin-bottom: 0;
+  font-size: 14px;
+}
+
+.picture-width{
+  width: 50%;
+  float: left;
+}
+
+@media only screen and (max-width: 1100px){
+  .logo{
+    width: 100%;
+  }
+}
+
+</style>
